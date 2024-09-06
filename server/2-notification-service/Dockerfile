597719c7d79bc820ceadf80bb20d00362db7b964
TaylorLi <mulhollanddrive2014@gmail.com>
2024-09-06 18:00:58 +0800

    feat: notification service dockerfile (#3)
    * feat:NMP-5 add tests for email send and consumer
    
    * chore:rename the folder
    
    * feat:docker files and fix build issue
    
    * fix
    
    ---------
    
    Co-authored-by: Taylor Li <258266864@qq.com>

---
+++ server/2-notification-service/Dockerfile
@@ -0,0 +1,25 @@
+FROM node:21-alpine3.18 as builder
+
+WORKDIR /app
+COPY package*.json ./
+COPY tsconfig.json ./
+COPY .npmrc ./
+COPY src ./src
+COPY tools ./tools
+RUN npm install -g pnpm
+RUN pnpm install --frozen-lockfile && pnpm run build
+
+FROM node:21-alpine3.18
+
+WORKDIR /app
+RUN apk add --no-cache curl
+COPY package*.json ./
+COPY tsconfig.json ./
+COPY .npmrc ./
+RUN npm install -g pm2 pnpm
+RUN pnpm install --frozen-lockfile --prod
+COPY --from=builder /app/build ./build
+
+EXPOSE 4001
+
+CMD [ "npm", "run", "start" ]
--- server/2-notification-service/src/server.ts
+++ server/2-notification-service/src/server.ts
@@ -1,12 +1,14 @@
 import http from 'http';
 
 import { Application } from 'express';
+import { Channel } from 'amqplib';
+import { IEmailMessageDetails } from '@taylordurden/jobber-shared';
 import { healthRoutes } from '@notifications/routes';
 import { checkConnection } from '@notifications/elasticsearch';
 import { createConnection } from '@notifications/queues/connection';
-import { Channel } from 'amqplib';
 import { consumeAuthEmailMessages, consumeOrderEmailMessages } from '@notifications/queues/email.consumer';
 import { getLogger } from '@notifications/helpers';
+import { config } from '@notifications/config';
 
 const SERVER_PORT = 4001;
 const log = getLogger('notificationServer', 'debug');
@@ -23,6 +25,7 @@ async function startQueues(): Promise<void> {
   const emailChannel: Channel = (await createConnection()) as Channel;
   await consumeAuthEmailMessages(emailChannel);
   await consumeOrderEmailMessages(emailChannel);
+  await testSendEmailsQueue(emailChannel);
 }
 
 function startElasticsearch(): void {
@@ -39,4 +42,30 @@ function startServer(app: Application): void {
   } catch (error) {
     log.log('error', 'NotificationService startServer() method:', error);
   }
+}
+
+async function testSendEmailsQueue(emailChannel: Channel) {
+  const resetLink = `${config.CLIENT_URL}/reset?v_token=12sad13ed1we`;
+  const messageDetails: IEmailMessageDetails = {
+    receiverEmail: `${config.SENDER_EMAIL}`,
+    resetLink: resetLink,
+    username: 'Taylor',
+    template: 'forgotPassword' // need to exact match folder name in emails directory
+  };
+  await emailChannel.assertExchange('jobber-email-notification', 'direct');
+  const emailContent = JSON.stringify(messageDetails);
+  emailChannel.publish('jobber-email-notification', 'auth-email', Buffer.from(emailContent));
+
+  const orderUrl = `${config.CLIENT_URL}/orders/1111`;
+  const orderMsg = {
+    receiverEmail: `${config.SENDER_EMAIL}`,
+    buyerUsername: 'Taylor',
+    title: 'MUMU NFT',
+    sellerUsername: 'jobber',
+    orderUrl: orderUrl,
+    template: 'orderDelivered'
+  };
+  await emailChannel.assertExchange('jobber-order-notification', 'direct');
+  const orderContent = JSON.stringify(orderMsg);
+  emailChannel.publish('jobber-order-notification', 'order-email', Buffer.from(orderContent));
 }

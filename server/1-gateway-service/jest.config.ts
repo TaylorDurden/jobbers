70f68350b0857a813fa3226a2b8d1e52cd121d9e
Taylor Li <258266864@qq.com>
2024-09-06 21:55:55 +0800

    feat:NMP-6 repo setup

--- server/1-gateway-service/jest.config.ts
+++ server/1-gateway-service/jest.config.ts
@@ -22,7 +22,7 @@ const config: Config.InitialOptions = {
   },
   coverageReporters: ['text-summary', 'lcov'],
   moduleNameMapper: {
-    '@gateway/(.*)': ['<rootDir>/src/$1']
+    '@notifications/(.*)': ['<rootDir>/src/$1']
   }
 };
 

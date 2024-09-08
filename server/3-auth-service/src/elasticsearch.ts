5ac742af4813b51fb70ab9ae614db940a2cc9235
Taylor Li <258266864@qq.com>
2024-09-09 01:33:11 +0800

    feat:NMP-7 connect to elasticsearch

---
+++ server/3-auth-service/src/elasticsearch.ts
@@ -0,0 +1,25 @@
+import { Client } from '@elastic/elasticsearch';
+import { ClusterHealthResponse } from '@elastic/elasticsearch/lib/api/types';
+import { config } from '@auth/config';
+import { getLogger } from '@auth/helpers';
+
+const log = getLogger('notificationElasticSearchServer', 'debug');
+
+export const elasticSearchClient = new Client({
+  node: `${config.ELASTIC_SEARCH_URL}`
+});
+
+export async function checkConnection(): Promise<void> {
+  let isConnected = false;
+  while (!isConnected) {
+    log.info('AuthService connecting to ElasticSearch...');
+    try {
+      const health: ClusterHealthResponse = await elasticSearchClient.cluster.health({});
+      log.info(`NotificationService Elasticsearch health status - ${health.status}`);
+      isConnected = true;
+    } catch (error) {
+      log.error('Connection to Elasticsearch failed. Retrying...');
+      log.log('error', 'NotificationService checkConnection() method:', error);
+    }
+  }
+}

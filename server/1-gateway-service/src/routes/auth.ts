fe760a677528713a6df929eda1f6db3a5ccf2591
Taylor Li <258266864@qq.com>
2024-09-10 03:43:09 +0800

    feat:NMP-7 signIn for auth service and gateway service

--- server/1-gateway-service/src/routes/auth.ts
+++ server/1-gateway-service/src/routes/auth.ts
@@ -1,3 +1,4 @@
+import { SignIn } from '@gateway/controllers/auth/signin';
 import { SignUp } from '@gateway/controllers/auth/signup';
 import express, { Router } from 'express';
 
@@ -9,8 +10,8 @@ class AuthRoutes {
   }
 
   public routes(): Router {
-    // localhost:4000/gateway-health
     this.router.post('/auth/signup', SignUp.prototype.create);
+    this.router.post('/auth/signin', SignIn.prototype.signIn);
     return this.router;
   }
 }
--- server/3-auth-service/src/routes/auth.ts
+++ server/3-auth-service/src/routes/auth.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express';
 import { create } from '@auth/controllers/signup';
+import { signIn } from '@auth/controllers/signin';
 
 const router: Router = express.Router();
 
 export function authRoutes(): Router {
   router.post('/signup', create);
+  router.post('/signin', signIn);
 
   return router;
 }

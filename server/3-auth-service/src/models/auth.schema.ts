a66fdd8725d6b942c5f160b1a392791be2e0176a
Taylor Li <258266864@qq.com>
2024-09-09 23:46:14 +0800

    feat:NMP-7 user service methods and signup user endpoint

--- server/3-auth-service/src/models/auth.schema.ts
+++ server/3-auth-service/src/models/auth.schema.ts
@@ -70,6 +70,10 @@ const AuthModel: ModelDefined<IAuthDocument, AuthUserCreationAttributes> & AuthM
       {
         unique: true,
         fields: ['username']
+      },
+      {
+        unique: true,
+        fields: ['emailVerificationToken']
       }
     ]
   }

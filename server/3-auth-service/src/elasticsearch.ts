e509ba21a8fb844ae4b328b303e57c18363a98ba
Taylor Li <258266864@qq.com>
2024-09-12 03:27:56 +0800

    feat:NMP-8 auth service search gigs api

--- server/3-auth-service/src/elasticsearch.ts
+++ server/3-auth-service/src/elasticsearch.ts
@@ -1,15 +1,16 @@
 import { Client } from '@elastic/elasticsearch';
-import { ClusterHealthResponse } from '@elastic/elasticsearch/lib/api/types';
+import { ClusterHealthResponse, GetResponse } from '@elastic/elasticsearch/lib/api/types';
 import { config } from '@auth/config';
 import { getLogger } from '@auth/helpers';
+import { ISellerGig } from '@taylordurden/jobber-shared';
 
-const log = getLogger('notificationElasticSearchServer', 'debug');
+const log = getLogger('authElasticSearchServer', 'debug');
 
-export const elasticSearchClient = new Client({
+const elasticSearchClient = new Client({
   node: `${config.ELASTIC_SEARCH_URL}`
 });
 
-export async function checkConnection(): Promise<void> {
+async function checkConnection(): Promise<void> {
   let isConnected = false;
   while (!isConnected) {
     log.info('AuthService connecting to ElasticSearch...');
@@ -23,3 +24,39 @@ export async function checkConnection(): Promise<void> {
     }
   }
 }
+
+async function checkIfIndexExists(indexName: string): Promise<boolean> {
+  const result: boolean = await elasticSearchClient.indices.exists({ index: indexName });
+  return result;
+}
+
+async function createIndex(indexName: string): Promise<void> {
+  try {
+    const existed: boolean = await checkIfIndexExists(indexName);
+    if (existed) {
+      log.info(`Index "${indexName}" already exist.`);
+    } else {
+      await elasticSearchClient.indices.create({ index: indexName });
+      await elasticSearchClient.indices.refresh({ index: indexName });
+      log.info(`Created index ${indexName}`);
+    }
+  } catch (error) {
+    log.error(`An error occurred while creating the index ${indexName}`);
+    log.log('error', 'AuthService createIndex() method error:', error);
+  }
+}
+
+async function getDocumentById(index: string, gigId: string): Promise<ISellerGig> {
+  try {
+    const result: GetResponse = await elasticSearchClient.get({
+      index,
+      id: gigId
+    });
+    return result._source as ISellerGig;
+  } catch (error) {
+    log.log('error', 'AuthService elastcisearch getDocumentById() method error:', error);
+    return {} as ISellerGig;
+  }
+}
+
+export { elasticSearchClient, checkConnection, createIndex, getDocumentById };
--- server/3-auth-service/src/routes.ts
+++ server/3-auth-service/src/routes.ts
@@ -2,13 +2,15 @@ import { Application } from 'express';
 import { healthRoutes } from '@auth/routes/health';
 import { authRoutes } from '@auth/routes/auth';
 import { verifyGatewayRequest } from '@taylordurden/jobber-shared';
-import { currentUserRoutes } from './routes/current-user';
+import { currentUserRoutes } from '@auth/routes/current-user';
+import { searchRoutes } from '@auth/routes/search';
 
 const BASE_PATH = '/api/v1/auth';
 
 export function appRoutes(app: Application): void {
   // http://localhost:4002/auth-health
   app.use('', healthRoutes());
+  app.use(BASE_PATH, searchRoutes());
   app.use(BASE_PATH, verifyGatewayRequest, authRoutes());
   app.use(BASE_PATH, verifyGatewayRequest, currentUserRoutes());
   // app.use(BASE_PATH, (req: Request) => {

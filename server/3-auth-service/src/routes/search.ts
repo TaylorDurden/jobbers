001a6fee9f81eb8fd374e92b0c51cc7e456a4c49
Taylor Li <258266864@qq.com>
2024-09-12 03:58:10 +0800

    feat:NMP-8 api gatway for gigs

--- server/3-auth-service/src/routes/search.ts
+++ server/3-auth-service/src/routes/search.ts
@@ -4,8 +4,8 @@ import express, { Router } from 'express';
 const router: Router = express.Router();
 
 export function searchRoutes(): Router {
-  router.get('/search/gig/:from/:size/:type', gigs);
-  router.get('/search/gig/:gigId', singleGigById);
+  router.get('/search/gigs/search', gigs);
+  router.get('/search/gigs/:gigId', singleGigById);
 
   return router;
 }

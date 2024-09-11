acf0199621643bd82da5e191655d3248528b98bb
Taylor Li <258266864@qq.com>
2024-09-12 04:55:19 +0800

    feat:NMP-8 auth service seed users api and test

--- server/3-auth-service/src/routes.ts
+++ server/3-auth-service/src/routes.ts
@@ -4,6 +4,7 @@ import { authRoutes } from '@auth/routes/auth';
 import { verifyGatewayRequest } from '@taylordurden/jobber-shared';
 import { currentUserRoutes } from '@auth/routes/current-user';
 import { searchRoutes } from '@auth/routes/search';
+import { seedRoutes } from '@auth/routes/seed';
 
 const BASE_PATH = '/api/v1/auth';
 
@@ -11,6 +12,8 @@ export function appRoutes(app: Application): void {
   // http://localhost:4002/auth-health
   app.use('', healthRoutes());
   app.use(BASE_PATH, searchRoutes());
+  app.use(BASE_PATH, seedRoutes());
+
   app.use(BASE_PATH, verifyGatewayRequest, authRoutes());
   app.use(BASE_PATH, verifyGatewayRequest, currentUserRoutes());
   // app.use(BASE_PATH, (req: Request) => {

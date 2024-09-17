8e7b0678fc4337b1a2c6886d91a87c7815538d57
Taylor Li <258266864@qq.com>
2024-09-18 03:13:02 +0800

    feat:NMP-10 finish gig service and gateway

--- server/1-gateway-service/src/controllers/auth/current-user.ts
+++ server/1-gateway-service/src/controllers/auth/current-user.ts
@@ -1,7 +1,11 @@
-import { authService } from '@gateway/services/api/auth.service';
 import { AxiosResponse } from 'axios';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { GatewayCache } from '@gateway/redis/gateway.cache';
+import { authService } from '@gateway/services/api/auth.service';
+import { socketIO } from '@gateway/server';
+
+const gatewayCache: GatewayCache = new GatewayCache();
 
 export class CurrentUser {
   public async getCurrentUser(_req: Request, res: Response): Promise<void> {
@@ -12,5 +16,17 @@ export class CurrentUser {
   public async resendEmail(req: Request, res: Response): Promise<void> {
     const response: AxiosResponse = await authService.resendEmail(req.body);
     res.status(StatusCodes.OK).json({ message: response.data.message, user: response.data.user });
+  }
+
+  public async getLoggedInUsers(_req: Request, res: Response): Promise<void> {
+    const response: string[] = await gatewayCache.getLoggedInUsersFromCache('loggedInUsers');
+    socketIO.emit('online', response);
+    res.status(StatusCodes.OK).json({ message: 'User is online' });
+  }
+
+  public async removeLoggedInUser(req: Request, res: Response): Promise<void> {
+    const response: string[] = await gatewayCache.removeLoggedInUserFromCache('loggedInUsers', req.params.username);
+    socketIO.emit('online', response);
+    res.status(StatusCodes.OK).json({ message: 'User is offline' });
   }
 }

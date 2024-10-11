7c190f87e352637670950586aca300d141369b44
Taylor Li <258266864@qq.com>
2024-10-11 12:39:00 +0800

    feat:combine RTK query

--- jobber-client/src/store/store.ts
+++ jobber-client/src/store/store.ts
@@ -2,6 +2,7 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { api } from './api';
 
 const persistConfig = {
   key: 'root',
@@ -9,7 +10,9 @@ const persistConfig = {
   blacklist: ['clientApi', '_persist']
 };
 
-export const combineReducer = combineReducers({});
+export const combineReducer = combineReducers({
+  [api.reducerPath]: api.reducer
+});
 
 export const rootReducers = (state, action) => {
   // this is to reset the state to default when user logs out
